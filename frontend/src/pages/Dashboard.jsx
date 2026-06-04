f14a9177807323b0d5df63dce7677405ea3ea78a
Dipteshsingh <diptesh.singh73@gmail.com>
2026-06-04 22:21:01 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
-  const baseUrl = "https://mern-assignment-7.onrender.com"
+  const baseUrl = "https://mern-assignment-10.onrender.com"
 
   const [tasks, setTasks] = useState([]);
   const [groupedTasks, setGroupedTasks] = useState({});
