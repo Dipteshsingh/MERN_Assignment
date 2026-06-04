942a67b3e6bc7982bd4db86b86f046906e13fe4c
Dipteshsingh <diptesh.singh73@gmail.com>
2026-06-04 22:01:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+
+  ],
+  credentials: true
+}));
+
 app.get('/', (req, res) => {
   res.send("Api working");
 })
