d99fd9f55ff1f1f5a69e266db5ce544d23e77c81
Dipteshsingh <diptesh.singh73@gmail.com>
2026-06-04 22:15:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({
   origin: [
     "http://localhost:5173",
-
+    "https://mern-assignment-9.onrender.com"
   ],
   credentials: true
 }));
