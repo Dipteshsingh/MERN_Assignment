ad932a3fedda1c7c626613c9f5b82df8c44f6a9f
Dipteshsingh <diptesh.singh73@gmail.com>
2026-06-04 22:05:54 +0530

    Update Upload.jsx

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 const Upload = () => {
-  const baseUrl = "https://mern-assignment-4-e1k0.onrender.com"
+  const baseUrl = "https://mern-assignment-7.onrender.com"
 
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
