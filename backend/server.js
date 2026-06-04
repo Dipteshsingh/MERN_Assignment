b2257eafb4a608000bfde4373fb1529497c63a73
Dipteshsingh <diptesh.singh73@gmail.com>
2026-06-04 22:25:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://mern-assignment-0.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send("Api working");
